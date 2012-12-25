eb280fe5ba05f8de54ac33b4079f8110f7207b66
Benjamin J. Balter <ben@balter.com>
2012-12-25 17:13:05 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source :rubygems
+
+gem 'sinatra'
+gem 'json'
+gem 'git'
+gem 'jekyll'
---
+++ server.rb
@@ -0,0 +1,51 @@
+require 'sinatra'
+require 'json'
+require 'git'
+require 'jekyll'
+
+post '/' do
+  dir = './tmp/jekyll'
+  username = ENV['GH_USER'] || ''
+  password = ENV['GH_PASS'] || ''
+  
+  push = JSON.parse(params[:payload])
+  url = push["repository"]["url"] + ".git"
+  url["https://"] = "https://" + username + ":" + password + "@"
+  
+  puts "cloning into " + url
+  g = Git.clone(url, dir)  
+  
+  options = {}
+  options["server"] = false
+  options["auto"] = false 
+  options["source"] = dir
+  options["destination"] = dir + "/_site"
+  options = Jekyll.configuration(options)
+  site = Jekyll::Site.new(options)
+  
+  puts "starting to build in " + dir
+  begin
+    site.process
+  rescue Jekyll::FatalException => e
+    puts e.message
+    FileUtils.rm_rf dir
+    exit(1)
+  end
+  
+  puts "succesfully built; commiting..."
+  begin
+    puts g.commit_all( "JekyllBot building JSON files")
+  rescue Git::GitExecuteError => e
+    puts e.message
+  else
+    puts "pushing"
+    puts g.push
+    puts "pushed"
+  end
+  
+  puts "cleaning up."
+  FileUtils.rm_rf dir
+  
+  puts "done"
+  
+end
